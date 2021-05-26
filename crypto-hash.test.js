d31118b729bfdb07fe09ed789b38bd92ea60a951
Kavin <17km50@queensu.ca>
2021-05-25 21:33:54 -0400

    small edits to crypto hash test file

--- crypto-hash.test.js
+++ crypto-hash.test.js
@@ -10,6 +10,6 @@ describe('cryptoHash()', () =>{
     });
 
     it('produces the same hash with the  same input arguments in any order', () =>{
-        expect(cryptoHash('one', 'two', 'three'))
+        expect(cryptoHash('one', 'two', 'three')).toEqual(cryptoHash('three', 'one', 'two'));
     })
 });

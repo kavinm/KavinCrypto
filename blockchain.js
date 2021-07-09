9ad2cdeeb215dfa9a77c8e842f85c869ef0c960e
kavinm <kavin1810@gmail.com>
2021-07-08 20:12:59 -0400

    chain validation isValidChain completed

--- blockchain.js
+++ blockchain.js
@@ -1,5 +1,6 @@
 
 const Block = require('./block');
+const cryptoHash = require('./crypto-hash'); 
 
 class Blockchain{
     constructor(){
@@ -15,7 +16,30 @@ class Blockchain{
         this.chain.push(newBlock);
     }
 
+    
     static isValidChain(chain){
+        //objects in js are not equal unless they are the same object
+        //json.stringify will check if it contains all the same data as genesis block
+        if (JSON.stringify(chain[0]) !== JSON.stringify( Block.genesis())) {
+            return false;
+        }
+
+        for(let i=1; i<chain.length; i++){
+            const block = chain[i]; //local block copied from chain
+
+            const actualLastHash = chain[i-1].hash;
+
+            const {timestamp, lastHash, hash, data } = block;
+
+            if (lastHash !== actualLastHash) return false; 
+
+            const validatedHash = cryptoHash(timestamp, lastHash, data);
+
+            if(hash !== validatedHash){
+                return false;
+            }
+        }
+
         return true;
     }
 }

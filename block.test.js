bec1665ebe7565c3c1ed10d28afb0d25fc0a6ac4
kavinm <kavin1810@gmail.com>
2021-07-12 19:23:00 -0400

    added jest mock functions for log and error

--- block.test.js
+++ block.test.js
@@ -1,77 +1,82 @@
-const Block = require('./block'); // requires Block class to exist in block.js
-const { GENESIS_DATA } = require('./config'); // requires genesis data to exist
-const cryptoHash = require('./crypto-hash');
-
+const Block = require("./block"); // requires Block class to exist in block.js
+const { GENESIS_DATA } = require("./config"); // requires genesis data to exist
+const cryptoHash = require("./crypto-hash");
 
 //describe creates the test case for the Block class
-describe('Block', () =>  { 
-    const timestamp = 'a-date';
-    const lastHash = 'foo-hash';
-    const hash = 'bar-hash';
-    const data = ['blockchain','data'];
+describe("Block", () => {
+    const timestamp = "a-date";
+    const lastHash = "foo-hash";
+    const hash = "bar-hash";
+    const data = ["blockchain", "data"];
+    const nonce = 1;
+    const difficulty = 1;
+
     const block = new Block({
-       timestamp, lastHash, hash, data
+        timestamp,
+        lastHash,
+        hash,
+        data,
+        nonce,
+        difficulty,
     });
 
-// it has same paramters as describe, used for testing to actually verify
-    it('has a timestamp, lastHash, hash, data property', () => { //
+    // it has same paramters as describe, used for testing to actually verify
+    it("has a timestamp, lastHash, hash, data property", () => {
+        //
         expect(block.timestamp).toEqual(timestamp);
         expect(block.lastHash).toEqual(lastHash);
-        expect(block.timestamp).toEqual(timestamp);
-        expect(block.timestamp).toEqual(timestamp);
+        expect(block.hash).toEqual(hash);
+        expect(block.data).toEqual(data);
+        expect(block.nonce).toEqual(nonce);
+        expect(block.difficulty).toEqual(difficulty);
     });
 
     //test case for genesis block
     //genesis function is a static function
-describe('genesis()', () =>{
-    const genesisBlock = Block.genesis();
+    describe("genesis()", () => {
+        const genesisBlock = Block.genesis();
 
-console.log('genesisBlock', genesisBlock );
+        console.log("genesisBlock", genesisBlock);
 
-    //checks if the genesisBlock is a block instance
-    it('returns a Block instance', () =>{
-        expect(genesisBlock instanceof Block).toBe(true); //toBe is used only for true/false (boolean values)
+        //checks if the genesisBlock is a block instance
+        it("returns a Block instance", () => {
+            expect(genesisBlock instanceof Block).toBe(true); //toBe is used only for true/false (boolean values)
+        });
+
+        //checks to see if the genesisBlock also contains the specific genesis data that is hard coded
+        it("returns the genesis data", () => {
+            expect(genesisBlock).toEqual(GENESIS_DATA);
+        });
+
+        //test for checking mineBlock
+        describe("mineBlock()", () => {
+            const lastBlock = Block.genesis(); // we are describing the mineBlock object
+            const data = "mined data"; //can be any data
+            const minedBlock = Block.mineBlock({ lastBlock, data }); //
+
+            it("returns a Block instance", () => {
+                expect(minedBlock instanceof Block).toBe(true);
+            });
+
+            it("sets the `lasthash` to be the `hash` of the lastBlock ", () => {
+                expect(minedBlock.lastHash).toEqual(lastBlock.hash); // we need to have the actual value in the expect statement and the expected value second for jest
+            }); // functionally either order will work but this is how it's done for tdd in jest
+
+            it("sets the `data`", () => {
+                expect(minedBlock.data).toEqual(data);
+            });
+
+            // check to see if timestamp is not undefined, can be a different value
+            it("sets a `timestamp`", () => {
+                expect(minedBlock.timestamp).not.toEqual(undefined);
+            });
+
+            //hash of minedBlock must equal sha256 of its timestamp, previousBlock's hash and the data
+            it("creates a SHA-256 `hash` based on the proper inputs", () => {
+                expect(minedBlock.hash).toEqual(
+                    cryptoHash(minedBlock.timestamp, lastBlock.hash, data)
+                );
+            });
+        });
     });
-
-    //checks to see if the genesisBlock also contains the specific genesis data that is hard coded
-    it('returns the genesis data', () =>{
-        expect(genesisBlock).toEqual(GENESIS_DATA);
-    })
-
-//test for checking mineBlock
-describe('mineBlock()', () =>{
-    const lastBlock = Block.genesis();          // we are describing the mineBlock object
-    const data = 'mined data';          //can be any data
-    const minedBlock = Block.mineBlock({lastBlock, data}); // 
-
-    it('returns a Block instance', () =>{
-        expect(minedBlock instanceof Block).toBe(true);
-    });
-
-    it('sets the `lasthash` to be the `hash` of the lastBlock ', () =>{
-        expect(minedBlock.lastHash).toEqual(lastBlock.hash); // we need to have the actual value in the expect statement and the expected value second for jest
-    });                                                     // functionally either order will work but this is how it's done for tdd in jest
-
-    it('sets the `data`', () =>{
-        expect(minedBlock.data).toEqual(data);
-    } );
-
-    // check to see if timestamp is not undefined, can be a different value
-    it('sets a `timestamp`', () =>{
-        expect(minedBlock.timestamp).not.toEqual(undefined); 
-    } );
-
-    //hash of minedBlock must equal sha256 of its timestamp, previousBlock's hash and the data 
-    it('creates a SHA-256 `hash` based on the proper inputs', () =>{    
-        expect(minedBlock.hash)
-            .toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data));    
-    })
 });
-
-});
-
-
-
-})
-
-
--- blockchain.test.js
+++ blockchain.test.js
@@ -1,117 +1,137 @@
-const Blockchain = require('./blockchain');
+const Blockchain = require("./blockchain");
 
-const Block = require('./block');
+const Block = require("./block");
 
-describe('Blockchain', () => {
+describe("Blockchain", () => {
     let blockchain, newChain, originalChain;
 
-    beforeEach(()=>{
+    beforeEach(() => {
         blockchain = new Blockchain(); //makes a new instance of blockchain object before each describe
         newChain = new Blockchain(); // will be used for chain replacement
 
         originalChain = blockchain.chain;
-        
     });
 
-    it('contains a `chain` Array instance', () =>{
+    it("contains a `chain` Array instance", () => {
         expect(blockchain.chain instanceof Array).toBe(true);
     });
 
-    it('starts with the genesis block', () =>{
+    it("starts with the genesis block", () => {
         expect(blockchain.chain[0]).toEqual(Block.genesis());
     });
 
-    it('adds a new block to the chain', () =>{
-        const newData = 'foo bar';
-        blockchain.addBlock({data: newData});// we are adding an object with fields that are contained a Block object
-        
-        expect(blockchain.chain[blockchain.chain.length-1].data).toEqual(newData);
+    it("adds a new block to the chain", () => {
+        const newData = "foo bar";
+        blockchain.addBlock({ data: newData }); // we are adding an object with fields that are contained a Block object
+
+        expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(
+            newData
+        );
     });
 
-    describe('isValidChain()', () =>{
-        describe('when the chain does not start with the genesis block', () => {
-            it('returns false', () =>{
-                blockchain.chain[0] = {data: 'fake-genesis'};
+    describe("isValidChain()", () => {
+        describe("when the chain does not start with the genesis block", () => {
+            it("returns false", () => {
+                blockchain.chain[0] = { data: "fake-genesis" };
 
                 expect(Blockchain.isValidChain(blockchain.chain)).toBe(false); // calling it on class as it is a static method, not on the instance
             });
         });
 
-
-        describe('when the chain starts with the genesis block and has multiple blocks', () =>{
-            beforeEach(() =>{
-                blockchain.addBlock({data: "Lions"});
-                blockchain.addBlock({data: "Tigers"}); // adding blocks  to the chain to test
-                blockchain.addBlock({data: "Zebras"});  //using before each so does not have to be done in each describe
+        describe("when the chain starts with the genesis block and has multiple blocks", () => {
+            beforeEach(() => {
+                blockchain.addBlock({ data: "Lions" });
+                blockchain.addBlock({ data: "Tigers" }); // adding blocks  to the chain to test
+                blockchain.addBlock({ data: "Zebras" }); //using before each so does not have to be done in each describe
             });
-            describe('and a lastHash reference has changed', () =>{
-                it('returns false', () => {                  
-                    blockchain.chain[2].lastHash = 'broken-lastHash'; 
-                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(false); // this should result in a broken lastHash reference
-                                
+            describe("and a lastHash reference has changed", () => {
+                it("returns false", () => {
+                    blockchain.chain[2].lastHash = "broken-lastHash";
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(
+                        false
+                    ); // this should result in a broken lastHash reference
                 });
             });
 
-            describe('and the chain contains a block with and invalid field', () =>{
-                it('returns false', () => {
-                    blockchain.chain[2].data = 'changed data'; // changing a field like this should invalidate the block
-                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+            describe("and the chain contains a block with and invalid field", () => {
+                it("returns false", () => {
+                    blockchain.chain[2].data = "changed data"; // changing a field like this should invalidate the block
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(
+                        false
+                    );
                 });
             });
 
-            describe('and the chain does not contain any invalid blocks', () =>{
-                it('returns true', () =>{
-                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
+            describe("and the chain does not contain any invalid blocks", () => {
+                it("returns true", () => {
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(
+                        true
+                    );
                 });
             });
         });
     });
 
-    describe('replaceChain()', () =>{
-        describe('when the new chain is not longer', () =>{
-            it('does not replace the chain', () => {
-                //the new chain is the same length but different
-                newChain.chain[0] = {new: 'chain'};
+    describe("replaceChain()", () => {
+        let errorMock;
+        let logMock;
+
+        beforeEach(() => {
+            errorMock = jest.fn(); //creates temporary methods for tests
+            logMock = jest.fn();
+
+            global.console.error = errorMock; // new functionality for replace chain log and error
+            global.console.log = logMock;
+        });
+
+        describe("when the new chain is not longer", () => {
+            beforeEach(() => {
+                newChain.chain[0] = { new: "chain" };
 
                 blockchain.replaceChain(newChain.chain);
-
-                expect (blockchain.chain).toEqual(originalChain);
             });
-                
+            it("does not replace the chain", () => {
+                //the new chain is the same length but different
+
+                expect(blockchain.chain).toEqual(originalChain);
+            });
+            it("logs an error", () => {
+                expect(errorMock).toHaveBeenCalled(); // check that an error has been logged, same as below
+            });
         });
-    
 
-        describe('when the new chain is longer', () =>{
-            beforeEach(() =>{
-                newChain.addBlock({data: "Lions"});
-                newChain.addBlock({data: "Tigers"}); // adding blocks  to the chain to test
-                newChain.addBlock({data: "Zebras"});
+        describe("when the new chain is longer", () => {
+            beforeEach(() => {
+                newChain.addBlock({ data: "Lions" });
+                newChain.addBlock({ data: "Tigers" }); // adding blocks  to the chain to test
+                newChain.addBlock({ data: "Zebras" });
             });
-            describe('and the chain is invalid', () =>{
-                it('does not replace the chain', () =>{
-                    newChain.chain[2].hash = '420xXfake-HashXx420';
+            describe("and the chain is invalid", () => {
+                beforeEach(() => {
+                    newChain.chain[2].hash = "420xXfake-HashXx420";
 
                     blockchain.replaceChain(newChain.chain);
-
+                });
+                it("does not replace the chain", () => {
                     expect(blockchain.chain).toEqual(originalChain);
                 });
+                it("logs an error", () => {
+                    expect(errorMock).toHaveBeenCalled(); // check that an error has been logged
+                });
             });
-        
-           describe('and the chain is valid', () =>{
-            it('replaces the chain', () =>{
-                blockchain.replaceChain(newChain.chain);
-                expect(blockchain.chain).toEqual(newChain.chain);
 
+            describe("and the chain is valid", () => {
+                beforeEach(() => {
+                    blockchain.replaceChain(newChain.chain);
+                });
+                it("replaces the chain", () => {
+                    expect(blockchain.chain).toEqual(newChain.chain);
+                });
+
+                it("logs about the chain replacement", () => {
+                    expect(logMock).toHaveBeenCalled(); // check console log has been called
+                });
             });
-           });
         });
-
     });
-        
-
-
-
-
-
-
 });

05e86b35865e055e188cfe815bd67ea1a755ea2a
kavinm <kavin1810@gmail.com>
2021-07-16 19:01:46 -0400

    added post request to add block

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const bodyParser = require("body-parser");
+
 const express = require("express");
 
 const Blockchain = require("./blockchain");
@@ -5,9 +7,23 @@ const Blockchain = require("./blockchain");
 const app = express();
 const blockchain = new Blockchain();
 
-//GET type HTTP request /request and response
+app.use(bodyParser.json());
+
+//GET type HTTP request
+//returns the blockchain and blocks
 app.get("/api/blocks", (req, res) => {
+    //request and response
     res.json(blockchain.chain);
+});
+
+//POST type HTTP request
+//lets you add blocks
+app.post("/api/mine", (req, res) => {
+    const { data } = req.body; //same as const data = req.body.data
+
+    blockchain.addBlock({ data });
+
+    res.redirect("/api/blocks");
 });
 
 const PORT = 3000;

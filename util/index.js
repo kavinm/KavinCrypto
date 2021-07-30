af257640ea7441e524e99ebcafe6dc50b7f34a95
kavinm <kavin1810@gmail.com>
2021-07-30 16:26:37 -0400

    added wallet verification and transaction tests

--- util/index.js
+++ util/index.js
@@ -1,7 +1,15 @@
 //elliptic curve
 const EC = require("elliptic").ec;
+const cryptoHash = require('./crypto-hash');
 
 //standards of efficient cryptography prime 256  Koblitz
 const ec = new EC('secp256k1');
 
-module.exports = {ec};
+const verifySignature = ({publicKey, data, signature}) =>{
+    //public key is encoded in hex, must specify
+    const keyFromPublic = ec.keyFromPublic(publicKey, 'hex');
+
+    return keyFromPublic.verify(cryptoHash(data), signature);
+};
+
+module.exports = {ec, verifySignature};
